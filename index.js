996d08f99eb8bccac4000f9d072d086a55c352f6
Maria <mmatrozzo@gmail.com>
2021-11-04 11:33:47 -0400

    commit tree d9c0adb22c98e96f163d1511decd13400b058e71

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+
+//   function superbowlWin(record) {
+//     if (record.result === "W") {
+//         return record.year;
+//     }
+// }
+
+// function superbowlWin(myArray) {
+//     if yearWin = myArray.find(element => element.result === "W") {
+//       return yearWin.year
+//     } else return undefined
+//   }
+//   superbowlWin(record)
+
+
+// function superbowlWin(record) {
+//     let result = record.find(record => {
+//         if (record.result === "W"){
+//         return record.year;
+//         }
+//     })
+//     return result.year;
+// }
+
+const sadReality = record.find(element => element.result === "W");
+const yearWon = record.find(element => element.year === "1969");
+
+function superbowlWin(record) {
+    if (sadReality) {
+        return yearWon;
+    } 
+}
